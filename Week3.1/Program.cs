a9e3ef239812d35476c1f081cedff8cea7a8f9ff
Matney0 <mlmoller21@amarillocollege.com>
2025-02-02 16:38:36 -0600

    Update Program.cs
    copying and pasting my code over to github, having issues pushing on my computer.

--- Week3.1/Program.cs
+++ Week3.1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Week3._1
 {
-    internal class Program
+    //Used class random to generate a number between 1 and 6
+    //Simulate dice roll
+    internal class RandomNumber
     {
-        static void Main(string[] args)
+
+        private static void Main(string[] args, bool shouldContinue)
+
         {
+         while (true)
+
+            Console.WriteLine("Welcome to the dice roll simulator, press any button to begin");
+
+            Console.ReadKey();
+            //Named the object dice and simulates a dice
+            //Generates a random number between 1 and 6
+            var dice = new Random();
+            int randomNumber = dice.Next(1, 6);
+            //Writes the result of the dice roll
+            Console.WriteLine("Your first dice roll resulted is " + randomNumber);
+
+
+            //starts the second dice roll
+            Console.WriteLine("And your second dice roll will ensue");
+
+            Console.ReadKey();
+
+            var dice2 = new Random();
+            int randomNumber2 = dice2.Next(1, 6);
+            //Writes the result of the dice roll
+            Console.WriteLine("Your second dice roll resulted is " + randomNumber2);
+
+
+            //Asks the user if they would like to roll again
+            Console.WriteLine("Would you like to roll again? Y/N");
+            if (Console.ReadLine().ToUpper() != "Y")
+            {
+                //ending the loop
+                break;
+            }
         }
     }
 }
